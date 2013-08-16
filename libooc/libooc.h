c2a65bfc9437e20dd1880c2c6430c9ebdf69d3c5
Timothy Yeh <timothyyeh@TimothytekiMacBook-Pro.local>
2013-08-16 20:00:50 +0800

    Initial Commit

---
+++ libooc/libooc.h
@@ -0,0 +1,13 @@
+//
+//  libooc.h
+//  libooc
+//
+//  Created by Timothy Yeh on 8/16/13.
+//  Copyright (c) 2013 Timothy Yeh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libooc : NSObject
+
+@end
---
+++ libooc/libooc.m
@@ -0,0 +1,13 @@
+//
+//  libooc.m
+//  libooc
+//
+//  Created by Timothy Yeh on 8/16/13.
+//  Copyright (c) 2013 Timothy Yeh. All rights reserved.
+//
+
+#import "libooc.h"
+
+@implementation libooc
+
+@end
